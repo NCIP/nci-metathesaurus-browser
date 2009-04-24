9c10768ea32b9a069284fe48757de541139b4f30
Will Garcia <garciaw>
2009-04-24 21:45:43 +0000

    Removed debug statements.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@65 b145b987-6716-48ef-8e4a-68a7679c2f12

--- software/ncimbrowser/src/java/gov/nih/nci/evs/browser/utils/RemoteServerUtil.java
+++ software/ncimbrowser/src/java/gov/nih/nci/evs/browser/utils/RemoteServerUtil.java
@@ -97,7 +97,7 @@ public class RemoteServerUtil {
             if (serviceUrl == null || serviceUrl.compareTo("") == 0)
             {
                 String lg_config_file = properties.getProperty(NCImBrowserProperties.LG_CONFIG_FILE);
-                if (true) {
+                if (debug) {
                         System.out.println(Utils.SEPARATOR);
                         System.out.println("LexBIGService(local): new LexBIGServiceImpl();");
                         System.out.println("LG_CONFIG_FILE: " + System.getProperty("LG_CONFIG_FILE"));
