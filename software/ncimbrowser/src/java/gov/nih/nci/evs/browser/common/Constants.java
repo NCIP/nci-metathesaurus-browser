8b4dd8745b56c43e279acc330695a9c1b7e79095
Kim Ong <ongki@mail.nih.gov>
2009-05-14 13:08:11 +0000

    Constants [KLO, 051409]
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@107 b145b987-6716-48ef-8e4a-68a7679c2f12

---
+++ software/ncimbrowser/src/java/gov/nih/nci/evs/browser/common/Constants.java
@@ -0,0 +1,28 @@
+package gov.nih.nci.evs.browser.common;
+
+/**
+ * Application constants class
+ * @author garciawa2
+ */
+public class Constants {
+
+    // Application version
+    static public final int MAJOR_VER = 1;
+    static public final int MINOR_VER = 0;
+    static public final String CONFIG_FILE = "NCImBrowserProperties.xml";
+    static public final String CODING_SCHEME_NAME = "NCI MetaThesaurus";
+
+	// Application constants
+	static public final String NA = "N/A";
+	static public final String TRUE = "true";
+	static public final String FALSE = "false";
+	static public final String EMPTY = "";
+
+	/**
+	 * Constructor
+	 */
+	private Constants() {
+		// Prevent class from being explicitly instantiated
+	}
+
+} // Class Constants
