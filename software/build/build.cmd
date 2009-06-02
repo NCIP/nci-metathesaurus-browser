4254e13cfc123f27efe559c27fc1d6ec914285fd
Will Garcia <garciaw>
2009-06-02 18:00:24 +0000

    Addition of clean and build all command.
    SVN-Revision: 147

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -8,6 +8,10 @@ if "%1" == "" (
     ant usage
     goto DONE
 )
+if "%1" == "all" (
+    ant build:all
+    goto DONE
+)
 if "%1" == "upgrade" (
     ant deploy:local:upgrade
     goto DONE
@@ -16,9 +20,17 @@ if "%1" == "deploy" (
     ant deploy:hot
     goto DONE
 )
+if "%1" == "clean" (
+    ant clean
+    if exist ..\target\*.* (
+       rmdir /Q /S ..\target
+    )
+    goto DONE
+)
 if "%1" == "dev" (
     ant -Dproperties.file=C:\SVN-Projects\ncim-properties\properties\dev-upgrade.properties deploy:remote:upgrade
     goto DONE
 )
+
 :DONE
 endlocal
