b79047c8dc9f4f89931326ed336aa5c598e1bcdd
David Yee <yeed>
2009-06-16 16:18:46 +0000

    Added flag to print in excel mode.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk/software/ncimbrowser@182 b145b987-6716-48ef-8e4a-68a7679c2f12

--- src/java/gov/nih/nci/evs/browser/utils/test/SearchUtilsTest.java
+++ src/java/gov/nih/nci/evs/browser/utils/test/SearchUtilsTest.java
@@ -7,6 +7,7 @@ import gov.nih.nci.evs.browser.utils.*;
 
 public class SearchUtilsTest extends SearchUtils {
     public static boolean isPerformanceTesting = false;
+    private static boolean isExcelFormat = false;
     
     public SearchUtilsTest(String url) {
         super(url);
@@ -15,46 +16,61 @@ public class SearchUtilsTest extends SearchUtils {
     public Vector<org.LexGrid.concepts.Concept> searchByName(String scheme,
         String version, String matchText, String source, String matchAlgorithm,
         int maxToReturn) {
-        debug(Utils.SEPARATOR);
-        debug("scheme = " + scheme);
-        debug("version = " + version);
-        debug("matchText = " + matchText);
-        debug("source = " + source);
-        debug("matchAlgorithm = " + matchAlgorithm);
-        debug("maxToReturn = " + maxToReturn);
+        debug(! isExcelFormat, Utils.SEPARATOR);
+        debug(! isExcelFormat, "scheme = " + scheme);
+        debug(! isExcelFormat, "version = " + version);
+        debug(! isExcelFormat, "matchText = " + matchText);
+        debug(! isExcelFormat, "source = " + source);
+        debug(! isExcelFormat, "matchAlgorithm = " + matchAlgorithm);
+        debug(! isExcelFormat, "maxToReturn = " + maxToReturn);
         return super.searchByName(scheme, version, matchText, source,
             matchAlgorithm, maxToReturn);
     }
 
-    protected void debug(String text) {
+    private void debug(String text) {
         if (isPerformanceTesting)
             System.out.println("PERF: " + text);
     }
 
-    public void testSearchByName() {
-        String scheme = "NCI MetaThesaurus";
-        String version = null;
-        String matchText = "blood";
-        String matchAlgorithm = "contains";
-        matchAlgorithm = "exactMatch";
-        int maxToReturn = 200;
+    private void debug(boolean display, String text) {
+        if (display)
+            debug(text);
+    }
 
+    public void search(String scheme, String version, String matchText,
+        String matchAlgorithm, int maxToReturn) {
         Utils.StopWatch stopWatch = new Utils.StopWatch();
         Vector<org.LexGrid.concepts.Concept> v = searchByName(scheme, version,
             matchText, matchAlgorithm, maxToReturn);
         String runtime = stopWatch.getResultInSeconds();
-        debug("Run time: " + runtime + " sec");
+        debug(! isExcelFormat, "Run time: " + runtime + " sec");
         if (v != null) {
-            debug("# of results: " + v.size());
+            debug(! isExcelFormat, "# of results: " + v.size());
             for (int i = 0; i < v.size(); i++) {
                 int j = i + 1;
                 Concept ce = (Concept) v.elementAt(i);
-                debug("(" + j + ")" + " " + ce.getEntityCode() + " "
+                debug(! isExcelFormat, "(" + j + ")" + " " + ce.getEntityCode() + " "
                     + ce.getEntityDescription().getContent());
             }
         }
-        debug("Excel: " + matchText + "\t" + matchAlgorithm + "\t" + runtime
-            + "\t" + v.size());
+        debug(isExcelFormat, "Excel: " + matchText + "\t" + matchAlgorithm + 
+            "\t" + runtime + "\t" + v.size());
+    }
+    
+    private void test1() {
+        String scheme = "NCI MetaThesaurus";
+        String version = null;
+        String matchAlgorithm = "contains";
+        matchAlgorithm = "exactMatch";
+        int maxToReturn = 200;
+
+        String[] matchTexts = new String[] {
+            "blood", "cell"
+        };
+
+        for (int i=0; i<matchTexts.length; ++i)
+            search(scheme, version, matchTexts[i], matchAlgorithm, maxToReturn);
+        debug("Done");
     }
 
     public static void main(String[] args) {
@@ -81,6 +97,6 @@ public class SearchUtilsTest extends SearchUtils {
         }
 
         SearchUtilsTest test = new SearchUtilsTest(url);
-        test.testSearchByName();
+        test.test1();
     }
 }
