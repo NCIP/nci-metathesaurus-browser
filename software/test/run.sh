7b1bf6e5a39f6f310c066f79d2da2ce880253de7
David Yee <yeed>
2009-06-16 14:38:02 +0000

    Added performance testing scripts for linux.
    SVN-Revision: 178

---
+++ software/test/run.sh
@@ -0,0 +1,22 @@
+#!/bin/csh -f
+
+cd ../ncimbrowser
+set CP=.
+set CP="$CP":conf
+set CP="$CP":build/web/WEB-INF/classes
+foreach lib (lib/*.jar)
+  set CP="$CP":"$lib"
+end
+setenv CLASSPATH $CP
+
+set java=java
+set class=gov.nih.nci.evs.browser.utils.SearchUtils
+#set config_file=-DLG_CONFIG_FILE=/local/content/evs/ncimbrowser50/config/lbconfig.props
+set args=(-propertyFile /home/evsuser/apps/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
+set args=($args -performanceTesting)
+
+set cmd = "$java $class $args"
+echo $cmd
+$cmd
+
+
