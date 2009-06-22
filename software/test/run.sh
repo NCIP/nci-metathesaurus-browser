f61f15112f000ff5055d44e75370b55fe16d4415
David Yee <yeed>
2009-06-22 14:27:45 +0000

    Modified script files to run "retrieving relationships" performance test.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@243 b145b987-6716-48ef-8e4a-68a7679c2f12

--- software/test/run.sh
+++ software/test/run.sh
@@ -7,10 +7,13 @@ set CP="$CP":build/web/WEB-INF/classes
 foreach lib (lib/*.jar)
   set CP="$CP":"$lib"
 end
+foreach lib (../test/lib/*.jar)
+  set CP="$CP":"$lib"
+end
 setenv CLASSPATH $CP
 
 set java=(java -Xms2048m -Xmx2048m)
-set class=gov.nih.nci.evs.browser.utils.test.SearchUtilsTest
+set class=gov.nih.nci.evs.browser.utils.test.MainTest
 set args=(-propertyFile /home/evsuser/apps/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
 
 set cmd = "$java $class $args"
