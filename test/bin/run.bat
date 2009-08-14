5a5442dd22236e32c6760b4eb7d356d3eaa905cd
David Yee <yeed>
2009-08-14 12:46:53 +0000

    Created scripts to compile and run test programs.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk/software/ncimbrowser@445 b145b987-6716-48ef-8e4a-68a7679c2f12

---
+++ test/bin/run.bat
@@ -0,0 +1,26 @@
+@echo off
+rem -------------------------------------------------------------------------------------
+setlocal enabledelayedexpansion
+set ocp=%CLASSPATH%
+
+cd ..
+set cp=.
+set cp=%cp%;..\conf
+set cp=%cp%;classes
+for %%x in (..\lib\*.jar ..\..\..\software\test\lib\*.jar) do (
+  set cp=!cp!;%%x
+)
+set CLASSPATH=%cp%
+
+set java=java
+set class=gov.nih.nci.evs.browser.utils.test.ResolveConceptIteratorTest
+set args=
+
+rem -------------------------------------------------------------------------------------
+@echo on
+%java% %class% %args%
+
+@echo off
+rem -------------------------------------------------------------------------------------
+set CLASSPATH=%ocp%
+@echo on
