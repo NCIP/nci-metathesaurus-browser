d3206fdf693433e22416c8f0d5091ced26453259
David Yee <yeed>
2009-08-14 15:48:33 +0000

    Created unix script to compile and run test programs.
    SVN-Revision: 449

---
+++ software/ncimbrowser/test/bin/compile.sh
@@ -0,0 +1,16 @@
+#!/bin/csh -f
+
+#----------------------------------------------------------------------------------------
+cd ..
+set cp=.
+foreach jar (../lib/*.jar)
+  set cp="$cp":"$jar"
+end
+setenv CLASSPATH $cp
+
+#----------------------------------------------------------------------------------------
+set javac=$JAVA_HOME/bin/javac
+
+#----------------------------------------------------------------------------------------
+mkdir -p classes
+$javac -d classes src/java/gov/nih/nci/evs/browser/utils/test/*.java
