0e646c80cff9ba61abcfd6cb232fe57c7dc1be9a
David Yee <yeed>
2009-08-14 12:56:13 +0000

    Added Main class in to test folder.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@447 b145b987-6716-48ef-8e4a-68a7679c2f12

---
+++ software/ncimbrowser/test/src/java/gov/nih/nci/evs/browser/utils/test/Main.java
@@ -0,0 +1,9 @@
+package gov.nih.nci.evs.browser.utils.test;
+
+public class Main {
+    public static void main(String[] args) throws Exception 
+    {
+        //RegExpTest.main(args);
+        ResolveConceptIteratorTest.main(args);
+    }
+}
