9254ef7dc99232b7c55194f21ffb89a9a7f3a33a
David Yee <yeed>
2009-10-23 14:15:19 +0000

    Updated scripts to point to /local/home/jboss45c.
    SVN-Revision: 513

--- software/test/run.sh
+++ software/test/run.sh
@@ -14,7 +14,8 @@ setenv CLASSPATH $CP
 
 set java=(java -Xms2048m -Xmx2048m)
 set class=gov.nih.nci.evs.browser.utils.test.MainTest
-set args=(-propertyFile /home/evsuser/apps/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
+#set args=(-propertyFile /home/evsuser/apps/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
+set args=(-propertyFile /local/home/jboss45c/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
 
 set cmd = "$java $class $args"
 echo $cmd
