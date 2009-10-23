8749a37fe55ea4c71a5305c5d0607e0686682ece
David Yee <yeed>
2009-10-23 17:31:42 +0000

    Modified Main to prompt all the other test program.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@517 b145b987-6716-48ef-8e4a-68a7679c2f12

--- software/ncimbrowser/test/src/java/gov/nih/nci/evs/browser/utils/test/Main.java
+++ software/ncimbrowser/test/src/java/gov/nih/nci/evs/browser/utils/test/Main.java
@@ -1,9 +1,21 @@
 package gov.nih.nci.evs.browser.utils.test;
 
 import java.util.*;
+import gov.nih.nci.evs.browser.utils.*;
 
 public class Main {
-    private static String[] parse(String[] args) {
+    private static String INDENT = "  ";
+    
+    private void println(String text) {
+        System.out.println(text);
+    }
+    
+    public Main(String[] args) {
+        args = parse(args);
+        prompt(args);
+    }
+    
+    private String[] parse(String[] args) {
         String prevArg = "";
         ArrayList<String> newArgs = new ArrayList<String>();
         for (int i = 0; i < args.length; ++i) {
@@ -21,11 +33,46 @@ public class Main {
         return newArgs.toArray(new String[newArgs.size()]);
     }
     
-    public static void main(String[] args) throws Exception 
-    {
-        args = parse(args);
-        // RegExpTest.main(args);
-        // ResolveConceptIteratorTest.main(args);
-        TestExtension.main(args);
+    public void prompt(String[] args) {
+        int choice = 1;
+        while (true) {
+            choice = prompt(choice);
+            if (choice == 0)
+                break;
+            if (! run(choice, args))
+                println("Invalid choice.  Try again.");
+        }
+        println("Quit");
+    }
+    
+    private int prompt(int choice) {
+        println(Utils.SEPARATOR);
+        println("Main Menu:");
+        println(INDENT + "1) " + "RegExpTest");
+        println(INDENT + "2) " + "ResolveConceptIteratorTest");
+        println(INDENT + "3) " + "TestExtension");
+        println(INDENT + "0) " + "Quit");
+        println(Utils.SEPARATOR);
+        choice = Prompt.prompt("Choose", choice);
+        return choice;
+    }
+    
+    private boolean run(int choice, String[] args) {
+        boolean returnValue = true;
+        try {
+            switch (choice) {
+                case 1: RegExpTest.main(args); break;
+                case 2: ResolveConceptIteratorTest.main(args); break;
+                case 3: TestExtension.main(args); break;
+                default: returnValue = false; break;
+            }
+            println("");
+        } catch (Exception e) {
+        }
+        return returnValue;
+    }
+
+    public static void main(String[] args) {
+        new Main(args);
     }
 }
