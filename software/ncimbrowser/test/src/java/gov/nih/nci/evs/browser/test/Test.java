3ed8ad7d6b23089344962cb7a20b42324e94c29d
David Yee <yeed>
2009-10-30 21:37:48 +0000

    Created Test program that prompts for LexEvsTest and PerformanceTest   test cases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@589 b145b987-6716-48ef-8e4a-68a7679c2f12

--- software/ncimbrowser/test/src/java/gov/nih/nci/evs/browser/test/Test.java
+++ software/ncimbrowser/test/src/java/gov/nih/nci/evs/browser/test/Test.java
@@ -1,21 +1,20 @@
-package gov.nih.nci.evs.browser.test.performance;
+package gov.nih.nci.evs.browser.test;
 
+import gov.nih.nci.evs.browser.test.lexevs.*;
+import gov.nih.nci.evs.browser.test.performance.*;
 import gov.nih.nci.evs.browser.test.utils.*;
 import gov.nih.nci.evs.browser.utils.*;
 
-public class PerformanceTest extends TestBase {
-    public PerformanceTest(int choice, String[] args) {
+public class Test extends TestBase {
+    public Test(int choice, String[] args) {
         super(choice, args);
     }
-    
+
     protected int prompt(int choice) {
         println(Utils.SEPARATOR);
         println("Main Menu:");
-        println(INDENT + "1) " + "SearchUtilsTest");
-        println(INDENT + "2) " + "RelationshipTest");
-        println(INDENT + "3) " + "RelationshipTest2");
-        println(INDENT + "4) " + "BySourceTest");
-        println(INDENT + "5) " + "BySourceTest2");
+        println(INDENT + "1) " + "LexEvsTest");
+        println(INDENT + "2) " + "PerformanceTest");
         println(INDENT + "0) " + "Quit");
         println(Utils.SEPARATOR);
         choice = Prompt.prompt("Choose", choice);
@@ -26,11 +25,8 @@ public class PerformanceTest extends TestBase {
         boolean returnValue = true;
         try {
             switch (choice) {
-                case 1: SearchUtilsTest.main(args); break;
-                case 2: RelationshipTest.main(args); break;
-                case 3: RelationshipTest2.main(args); break;
-                case 4: BySourceTest.main(args); break;
-                case 5: BySourceTest2.main(args); break;
+                case 1: LexEvsTest.main(args); break;
+                case 2: PerformanceTest.main(args); break;
                 default: returnValue = false; break;
             }
             println("");
@@ -41,6 +37,6 @@ public class PerformanceTest extends TestBase {
     }
 
     public static void main(String[] args) {
-        new PerformanceTest(3, args);
-    }    
+        new Test(2, args);
+    }
 }
