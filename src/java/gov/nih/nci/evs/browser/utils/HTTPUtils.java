f929875f004333539f9483545a8e4dcb5c98c43a
Will Garcia <garciaw>
2010-04-09 17:34:36 +0000

    Code needed for GF[#27754] Add HTTP Referer header information to search log.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk/software/ncimbrowser@808 b145b987-6716-48ef-8e4a-68a7679c2f12

--- src/java/gov/nih/nci/evs/browser/utils/HTTPUtils.java
+++ src/java/gov/nih/nci/evs/browser/utils/HTTPUtils.java
@@ -2,6 +2,7 @@ package gov.nih.nci.evs.browser.utils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.regex.Pattern;
 
 import javax.faces.context.FacesContext;
@@ -9,78 +10,111 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * HTTP Utility methods
+ * 
  * @author garciawa2
- *
+ * 
  */
 public class HTTPUtils {
 
-    /**
-	 * Remove potentially bad XSS syntax 
-	 * @param value
-	 * @return
-	 */
-	public static String cleanXSS(String value) {
+  /**
+   * Remove potentially bad XSS syntax
+   * 
+   * @param value
+   * @return
+   */
+  public static String cleanXSS(String value) {
 
-		if (value == null || value.length() < 1)
-			return value;
-		
-		try {
-			value = URLDecoder.decode(value, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			// Do nothing, just use the input
-        } catch (IllegalArgumentException e) {
-            // Do nothing, just use the input
-            // Note: The following exception was triggered:
-            //   java.lang.IllegalArgumentException: URLDecoder: Illegal hex
-            //     characters in escape (%) pattern - For input string: "^&".
-        }
-        
-		// Remove XSS attacks
-		value = replaceAll(value,"<\\s*script\\s*>.*</\\s*script\\s*>", "");		
-		value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-		value = value.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");
-		value = value.replaceAll("'", "&#39;");
-		value = value.replaceAll("eval\\((.*)\\)", "");
-		value = replaceAll(value,"[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");		
-		value = value.replaceAll("\"", "&quot;");
-		return value;
+    if (value == null || value.length() < 1)
+      return value;
 
-	}
-    
-	/**
-	 * @param string
-	 * @param regex
-	 * @param replaceWith
-	 * @return
-	 */
-	public static String replaceAll(String string, String regex, String replaceWith) {
-		
-		Pattern myPattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-		string = myPattern.matcher(string).replaceAll(replaceWith);
-		return string;
-		
-	}
-	
-	public static HttpServletRequest getRequest() {
-	    return (HttpServletRequest) FacesContext.getCurrentInstance().
-	        getExternalContext().getRequest();
-	}
-	
-    public static String getAttribute(HttpServletRequest request,
-        String attributeName, String defaultValue) {
-        String value = (String) request.getAttribute(attributeName);
-        return getValue(value, defaultValue);
+    try {
+      value = URLDecoder.decode(value, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      // Do nothing, just use the input
+    } catch (IllegalArgumentException e) {
+      // Do nothing, just use the input
+      // Note: The following exception was triggered:
+      // java.lang.IllegalArgumentException: URLDecoder: Illegal hex
+      // characters in escape (%) pattern - For input string: "^&".
     }
 
-    public static String getSessionAttribute(HttpServletRequest request,
-	    String attributeName, String defaultValue) {
-	    String value = (String) request.getSession().getAttribute(attributeName);
-	    return getValue(value, defaultValue);
-	}
-	
-	public static String getValue(String value, String defaultValue) {
-	    if (value == null || value.trim().length() <= 0 || value.equals("null"))
-	        return defaultValue;
-	    return value;
-	}
+    // Remove XSS attacks
+    value = replaceAll(value, "<\\s*script\\s*>.*</\\s*script\\s*>", "");
+    value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    value = value.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");
+    value = value.replaceAll("'", "&#39;");
+    value = value.replaceAll("eval\\((.*)\\)", "");
+    value = replaceAll(value, "[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+    value = value.replaceAll("\"", "&quot;");
+    return value;
+
+  }
+
+  /**
+   * @param string
+   * @param regex
+   * @param replaceWith
+   * @return
+   */
+  public static String replaceAll(String string, String regex, String replaceWith) {
+
+    Pattern myPattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+    string = myPattern.matcher(string).replaceAll(replaceWith);
+    return string;
+
+  }
+
+  public static HttpServletRequest getRequest() {
+    return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+  }
+
+  public static String getAttribute(HttpServletRequest request, String attributeName, String defaultValue) {
+    String value = (String) request.getAttribute(attributeName);
+    return getValue(value, defaultValue);
+  }
+
+  public static String getSessionAttribute(HttpServletRequest request, String attributeName, String defaultValue) {
+    String value = (String) request.getSession().getAttribute(attributeName);
+    return getValue(value, defaultValue);
+  }
+
+  public static String getValue(String value, String defaultValue) {
+    if (value == null || value.trim().length() <= 0 || value.equals("null"))
+      return defaultValue;
+    return value;
+  }
+  
+  /**
+   * @param request
+   * @return
+   */
+  public static String getRefererParmEncode(HttpServletRequest request) {
+    String iref = request.getHeader("Referer");
+    String referer = "N/A";
+    if (iref != null)
+      try {
+        referer = URLEncoder.encode(iref,"UTF-8");
+      } catch (UnsupportedEncodingException e) {
+        // return N/A if encoding is not supported.
+      }
+    return cleanXSS(referer);
+  }
+
+  /**
+   * @param request
+   * @return
+   */
+  public static String getRefererParmDecode(HttpServletRequest request) {
+    String refurl = "N/A";
+    try {
+      String iref = request.getParameter("referer");
+      if (iref != null)
+        refurl = URLDecoder.decode(request.getParameter("referer"),
+            "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      // return N/A if encoding is not supported.
+    }
+    return cleanXSS(refurl);
+  }  
+  
 }
