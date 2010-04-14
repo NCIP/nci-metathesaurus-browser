0c0d31f38233197408b1cd0a2d427ce95baf48a7
Will Garcia <garciaw>
2010-04-14 19:20:24 +0000

    Reformatted this page for readability.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@822 b145b987-6716-48ef-8e4a-68a7679c2f12

--- software/ncimbrowser/src/java/gov/nih/nci/evs/searchlog/SearchLevel.java
+++ software/ncimbrowser/src/java/gov/nih/nci/evs/searchlog/SearchLevel.java
@@ -4,26 +4,26 @@ import org.apache.log4j.Level;
 
 public class SearchLevel extends Level {
 
-	private static final long serialVersionUID = 1L;
-	
-	// High enough so only SL events get logged in the file
-	public static final int SEARCH_LOG_INT = FATAL_INT + 1000; 
-	public static final SearchLevel SEARCH_LOG_LEVEL = new SearchLevel(SEARCH_LOG_INT,"SEARCH",7);
-	
-	protected SearchLevel(int level, String levelStr, int syslogEquivalent) {
-		super(level, levelStr, syslogEquivalent);
-	}
+  private static final long serialVersionUID = 1L;
 
-    public static SearchLevel toLevel(String sArg) {
-    	return SEARCH_LOG_LEVEL;
-    }
-	
-    public static SearchLevel toLevel(int val) {
-    	return SEARCH_LOG_LEVEL;
-    }    
+  // High enough so only SL events get logged in the file
+  public static final int SEARCH_LOG_INT = FATAL_INT + 1000;
+  public static final SearchLevel SEARCH_LOG_LEVEL = new SearchLevel(SEARCH_LOG_INT, "SEARCH", 7);
 
-    public static Level toLevel(int val, Level defaultLevel) {
-    	return SEARCH_LOG_LEVEL;
-    }   
-    
+  protected SearchLevel(int level, String levelStr, int syslogEquivalent) {
+    super(level, levelStr, syslogEquivalent);
+  }
+
+  public static SearchLevel toLevel(String sArg) {
+    return SEARCH_LOG_LEVEL;
+  }
+
+  public static SearchLevel toLevel(int val) {
+    return SEARCH_LOG_LEVEL;
+  }
+
+  public static Level toLevel(int val, Level defaultLevel) {
+    return SEARCH_LOG_LEVEL;
+  }
+
 }
