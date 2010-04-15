12cb632c8955b963b1edec8144c7491d187d82e0
Will Garcia <garciaw>
2010-04-15 16:24:35 +0000

    Code cleanup.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk/software/ncimbrowser@829 b145b987-6716-48ef-8e4a-68a7679c2f12

--- src/java/gov/nih/nci/evs/searchlog/SearchLog.java
+++ src/java/gov/nih/nci/evs/searchlog/SearchLog.java
@@ -62,13 +62,20 @@ public class SearchLog {
 	/**
 	 * @param term
 	 */
-	public static void writeEntry(String term, String type, String target,
+	public static void writeEntry(String searchType, String term, String type, String target,
 			String source, int count, String referrer) {
 		init();
-		// "TERM|TYPE|TARGET|SOURCE|COUNT|REFERRER"
-		logger.log(SearchLevel.SEARCH_LOG_LEVEL, term + SEPARATOR + type
-				+ SEPARATOR + target + SEPARATOR + source + SEPARATOR + count
-				+ SEPARATOR + referrer);
+		// "SEARCH_TYPE|TERM|TYPE|TARGET|SOURCE|COUNT|REFERRER"
+		//    SEARCH_TYPE (Null=Simple, value=Advance search		 
+		
+		logger.log(SearchLevel.SEARCH_LOG_LEVEL,
+		    searchType + SEPARATOR +
+		    term + SEPARATOR +
+		    type + SEPARATOR +
+		    target + SEPARATOR +
+		    source + SEPARATOR +
+		    count	+ SEPARATOR +
+		    referrer);
 	}
 
 }
