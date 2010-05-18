ee180ae246b56970ea3c029956ad0bf2472e1db5
David Yee <yeed>
2010-05-18 15:21:20 +0000

    Reformatted source code and copyright notice.  Changed imports to package   for all classes in the utils package.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk/software/ncimbrowser@949 b145b987-6716-48ef-8e4a-68a7679c2f12

--- src/java/gov/nih/nci/evs/browser/utils/ScoredIterator.java
+++ src/java/gov/nih/nci/evs/browser/utils/ScoredIterator.java
@@ -1,19 +1,53 @@
 package gov.nih.nci.evs.browser.utils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
-import org.LexGrid.LexBIG.DataModel.Collections.ResolvedConceptReferenceList;
-import org.LexGrid.LexBIG.DataModel.Core.ResolvedConceptReference;
-import org.LexGrid.LexBIG.Exceptions.LBException;
-import org.LexGrid.LexBIG.Exceptions.LBInvocationException;
-import org.LexGrid.LexBIG.Exceptions.LBParameterException;
-import org.LexGrid.LexBIG.Exceptions.LBResourceUnavailableException;
-import org.LexGrid.LexBIG.Utility.Iterators.ResolvedConceptReferencesIterator;
+import org.LexGrid.LexBIG.DataModel.Collections.*;
+import org.LexGrid.LexBIG.DataModel.Core.*;
+import org.LexGrid.LexBIG.Exceptions.*;
+import org.LexGrid.LexBIG.Utility.Iterators.*;
+
+/**
+ * <!-- LICENSE_TEXT_START -->
+ * Copyright 2008,2009 NGIT. This software was developed in conjunction 
+ * with the National Cancer Institute, and so to the extent government 
+ * employees are co-authors, any rights in such works shall be subject 
+ * to Title 17 of the United States Code, section 105.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright 
+ *      notice, this list of conditions and the disclaimer of Article 3, 
+ *      below. Redistributions in binary form must reproduce the above 
+ *      copyright notice, this list of conditions and the following 
+ *      disclaimer in the documentation and/or other materials provided 
+ *      with the distribution.
+ *   2. The end-user documentation included with the redistribution, 
+ *      if any, must include the following acknowledgment:
+ *      "This product includes software developed by NGIT and the National 
+ *      Cancer Institute."   If no such end-user documentation is to be
+ *      included, this acknowledgment shall appear in the software itself,
+ *      wherever such third-party acknowledgments normally appear.
+ *   3. The names "The National Cancer Institute", "NCI" and "NGIT" must 
+ *      not be used to endorse or promote products derived from this software.
+ *   4. This license does not authorize the incorporation of this software
+ *      into any third party proprietary programs. This license does not 
+ *      authorize the recipient to use any trademarks owned by either NCI 
+ *      or NGIT 
+ *   5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED 
+ *      WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ *      OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE) ARE 
+ *      DISCLAIMED. IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE,
+ *      NGIT, OR THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ *      INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ *      BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ *      LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ *      CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ *      LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ *      ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *      POSSIBILITY OF SUCH DAMAGE.
+ * <!-- LICENSE_TEXT_END -->
+ */
 
 /**
  * Used to wrap scored results for consumption as a standard
@@ -26,9 +60,10 @@ public class ScoredIterator implements ResolvedConceptReferencesIterator {
     int scrolled = 0;
 
     /**
-     * Construct from a map of ScoredTerms, ordered from high to low score.
-     * If > 0, the maximum items parameter sets an upper limit on the number
-     * of top-scored items to maintain and return.
+     * Construct from a map of ScoredTerms, ordered from high to low score. If >
+     * 0, the maximum items parameter sets an upper limit on the number of
+     * top-scored items to maintain and return.
+     * 
      * @param scoredTerms
      * @param maxItems
      */
@@ -39,15 +74,19 @@ public class ScoredIterator implements ResolvedConceptReferencesIterator {
         Collections.sort(temp);
 
         // Maintain only as many items as the specified maximum ...
-        int limit = maxItems > 0 ? Math.min(maxItems, scoredTerms.size()) : scoredTerms.size();
+        int limit =
+            maxItems > 0 ? Math.min(maxItems, scoredTerms.size()) : scoredTerms
+                .size();
         int count = 0;
         this.scoredTerms = new ScoredTerm[limit];
-        for (Iterator<ScoredTerm> terms = temp.listIterator(); terms.hasNext() && count < limit; count++)
+        for (Iterator<ScoredTerm> terms = temp.listIterator(); terms.hasNext()
+            && count < limit; count++)
             this.scoredTerms[count] = terms.next();
     }
 
     /**
      * Construct from a pre-sorted array of ScoredTerms.
+     * 
      * @param scoredTerms
      */
     public ScoredIterator(ScoredTerm[] scoredTerms) {
@@ -57,10 +96,12 @@ public class ScoredIterator implements ResolvedConceptReferencesIterator {
     /**
      * Returns a specific range of items without altering cursor position.
      */
-    public ResolvedConceptReferenceList get(int start, int end) throws LBResourceUnavailableException,
-            LBInvocationException, LBParameterException {
+    public ResolvedConceptReferenceList get(int start, int end)
+            throws LBResourceUnavailableException, LBInvocationException,
+            LBParameterException {
         verifyResources();
-        ResolvedConceptReferenceList result = new ResolvedConceptReferenceList();
+        ResolvedConceptReferenceList result =
+            new ResolvedConceptReferenceList();
         int stop = Math.max(0, Math.min(scoredTerms.length, end));
         if (start < 0 || stop < start)
             throw new LBParameterException("Index out of bounds.");
@@ -86,7 +127,8 @@ public class ScoredIterator implements ResolvedConceptReferencesIterator {
     /**
      * Returns the next item and advances the cursor.
      */
-    public ResolvedConceptReference next() throws LBResourceUnavailableException, LBInvocationException {
+    public ResolvedConceptReference next()
+            throws LBResourceUnavailableException, LBInvocationException {
         verifyResources();
         return hasNext() ? scoredTerms[position++].ref : null;
     }
@@ -94,12 +136,13 @@ public class ScoredIterator implements ResolvedConceptReferencesIterator {
     /**
      * Returns the next 'n' items and advances the cursor.
      */
-    public ResolvedConceptReferenceList next(int maxToReturn) throws LBResourceUnavailableException,
-            LBInvocationException {
+    public ResolvedConceptReferenceList next(int maxToReturn)
+            throws LBResourceUnavailableException, LBInvocationException {
         verifyResources();
         ResolvedConceptReferenceList result = null;
         try {
-            int pageSize = Math.max(0, Math.min(maxToReturn, numberRemaining()));
+            int pageSize =
+                Math.max(0, Math.min(maxToReturn, numberRemaining()));
             result = get(position, position + pageSize);
             position += pageSize;
         } catch (LBParameterException e) {
@@ -113,8 +156,8 @@ public class ScoredIterator implements ResolvedConceptReferencesIterator {
      * Skips 'n' items which are available via getNext() until a call to next(),
      * returning self.
      */
-    public ResolvedConceptReferencesIterator scroll(int maxToReturn) throws LBResourceUnavailableException,
-            LBInvocationException {
+    public ResolvedConceptReferencesIterator scroll(int maxToReturn)
+            throws LBResourceUnavailableException, LBInvocationException {
         verifyResources();
         scrolled = Math.max(0, Math.min(maxToReturn, numberRemaining()));
         position += scrolled;
@@ -149,6 +192,7 @@ public class ScoredIterator implements ResolvedConceptReferencesIterator {
      */
     protected void verifyResources() throws LBResourceUnavailableException {
         if (scoredTerms == null)
-            throw new LBResourceUnavailableException("Iterator resources released.");
+            throw new LBResourceUnavailableException(
+                "Iterator resources released.");
     }
 }
