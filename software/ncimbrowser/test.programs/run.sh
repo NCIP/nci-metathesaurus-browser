2877d6b3d9c69da1876c6a3f5b7c8ef655e58403
David Yee <yeed>
2010-07-16 18:01:21 +0000

    GF28946: Moved ContainsRelationshipTest program into separate test program folder.
    SVN-Revision: 1049

---
+++ software/ncimbrowser/test.programs/run.sh
@@ -0,0 +1,21 @@
+#!/bin/csh -f
+
+#----------------------------------------------------------------------------------------
+set cp=.
+set cp="$cp":../conf
+set cp="$cp":classes
+foreach jar (../lib/*.jar lib/*.jar)
+  set cp="$cp":$jar
+end
+set cp="$cp":../build/web/WEB-INF/classes
+setenv CLASSPATH $cp
+
+#----------------------------------------------------------------------------------------
+set java=$JAVA_HOME/bin/java
+set class=gov.nih.nci.evs.browser.test.Test
+# set args=(-propertyFile /home/evsuser/apps/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
+set args=(-propertyFile /local/home/jboss45c/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
+
+
+#----------------------------------------------------------------------------------------
+$java $class $args
