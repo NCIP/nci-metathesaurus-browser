ef5d0c5132b1f130c88bae9ab5ca098632e99a3a
David Yee <yeed>
2010-07-19 15:21:27 +0000

    GF28946: Added test program provided by Kevin.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@1051 b145b987-6716-48ef-8e4a-68a7679c2f12

--- software/ncimbrowser/test.programs/run.sh
+++ software/ncimbrowser/test.programs/run.sh
@@ -12,10 +12,10 @@ setenv CLASSPATH $cp
 
 #----------------------------------------------------------------------------------------
 set java=$JAVA_HOME/bin/java
-set class=gov.nih.nci.evs.browser.test.Test
+set class=gov.nih.nci.evs.browser.test.KevinTest
 # set args=(-propertyFile /home/evsuser/apps/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
-set args=(-propertyFile /local/home/jboss45c/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
-
+# set args=(-propertyFile /local/home/jboss45c/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
+set args=(-propertyFile ~/apps/evs/ncim-webapp/conf/NCImBrowserProperties.xml)
 
 #----------------------------------------------------------------------------------------
 $java $class $args
