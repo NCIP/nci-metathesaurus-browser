9ec8e2d36a4e3891b3cca893c8d1b079898b89e4
Will Garcia <garciawa2>
2010-10-05 14:35:48 +0000

    Upgrade to JBoss 5.1, JSF 1.2. Lexevs is still 5.1.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/trunk@1099 b145b987-6716-48ef-8e4a-68a7679c2f12

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -3,10 +3,17 @@
 @rem **** Command file to invoke build.xml ****
 @rem ******************************************
 setlocal
+@rem Environment settings here...
 set DEVPROPFILE=C:\NCI-Projects\ncim-properties\properties\dev-upgrade.properties
 set CIPROPFILE=C:\NCI-Projects\ncim-properties\properties\ci-upgrade.properties
 set QAPROPFILE=C:\NCI-Projects\ncim-properties\properties\qa-upgrade.properties
 set DATAQAPROPFILE=C:\NCI-Projects\ncim-properties\properties\data-qa-upgrade.properties
+set DEBUG=-Denable.install.debug=false
+set TAG=-Danthill.build.tag_built=desktop
+@rem Test is debug has been set
+if "%2" == "debug" (
+    set DEBUG=-Denable.install.debug=true -debug
+)
 cls
 if "%1" == "" (
     echo.
@@ -21,30 +28,46 @@ if "%1" == "" (
     echo   ci           -- Builds, upgrades JBoss on CI
     echo   qa           -- Builds, upgrades JBoss on QA
     echo   data-qa      -- Builds, upgrades JBoss on Data QA
-    echo   deploy       -- Redeploy application
+    echo   deploy       -- Hot deploy application
+    echo   jsp          -- Hot deploy JSP files
+    echo   stop         -- Stop war file
+    echo   start        -- Start war file
+    echo   cissh        -- Test SSH login in CI
     goto DONE
 )
 if "%1" == "all" (
-    ant -Danthill.build.tag_built=desktop build:all
+    ant %TAG% build:all
     goto DONE
 )
 if "%1" == "upgrade" (
-    ant -Danthill.build.tag_built=desktop deploy:local:upgrade
-    goto DONE
-)
-if "%1" == "deploy" (
-    ant -Danthill.build.tag_built=desktop deploy:hot
+    ant %TAG% %DEBUG% deploy:local:upgrade
     goto DONE
 )
 if "%1" == "install" (
-    ant -Danthill.build.tag_built=desktop deploy:local:install
+    ant %TAG% %DEBUG% deploy:local:install
+    goto DONE
+)
+if "%1" == "deploy" (
+    ant %TAG% %DEBUG% deploy:hot
+    goto DONE
+)
+if "%1" == "stop" (
+    ant %TAG% %DEBUG% deploy:stop
+    goto DONE
+)
+if "%1" == "start" (
+    ant %TAG% %DEBUG% deploy:start
+    goto DONE
+)
+if "%1" == "jsp" (
+    ant %DEBUG% deploy:hot:jsp
     goto DONE
 )
 if "%1" == "clean" (
+    ant clean
     if exist ..\target\*.* (
        rmdir /Q /S ..\target
     )
-    ant clean
     goto DONE
 )
 if "%1" == "reconfig" (
@@ -52,19 +75,23 @@ if "%1" == "reconfig" (
     goto DONE
 )
 if "%1" == "dev" (
-    ant -Dproperties.file=%DEVPROPFILE% -Danthill.build.tag_built=desktop deploy:remote:upgrade
+    ant -Dproperties.file=%DEVPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
     goto DONE
 )
 if "%1" == "ci" (
-    ant -Dproperties.file=%CIPROPFILE% -Danthill.build.tag_built=desktop deploy:remote:upgrade
+    ant -Dproperties.file=%CIPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
     goto DONE
 )
 if "%1" == "qa" (
-    ant -Dproperties.file=%QAPROPFILE% -Danthill.build.tag_built=desktop deploy:remote:upgrade
+    ant -Dproperties.file=%QAPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
     goto DONE
 )
 if "%1" == "data-qa" (
-    ant -Dproperties.file=%DATAQAPROPFILE% -Danthill.build.tag_built=desktop deploy:remote:upgrade
+    ant -Dproperties.file=%DATAQAPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
+    goto DONE
+)
+if "%1" == "cissh" (
+    ssh jboss51a@ncias-c512-v.nci.nih.gov -i C:\NCI-Projects\ncim-properties\properties\ssh-keys\id_dsa_bda echo "Test worked!"
     goto DONE
 )
 :DONE
