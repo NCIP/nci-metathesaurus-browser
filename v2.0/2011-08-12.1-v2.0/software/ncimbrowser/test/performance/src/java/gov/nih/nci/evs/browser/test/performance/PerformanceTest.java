aa888b1d4fc2f5394bc6862d617c7a28c590ea5f
David Yee <yeed>
2012-02-23 14:00:28 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncim/tags/_Archived@1345 b145b987-6716-48ef-8e4a-68a7679c2f12

---
+++ v2.0/2011-08-12.1-v2.0/software/ncimbrowser/test/performance/src/java/gov/nih/nci/evs/browser/test/performance/PerformanceTest.java
@@ -0,0 +1,34 @@
+package gov.nih.nci.evs.browser.test.performance;
+
+import gov.nih.nci.evs.browser.test.utils.*;
+
+public class PerformanceTest extends TestBase {
+    public PerformanceTest(int choice, String[] args) {
+        super(choice, args);
+    }
+    
+    protected void displayOptions() {
+        println(getClass().getSimpleName() + " Menu:");
+        println(INDENT + "1) " + "SearchUtilsTest");
+        println(INDENT + "2) " + "RelationshipTest");
+        println(INDENT + "3) " + "RelationshipTest2");
+        println(INDENT + "4) " + "BySourceTest");
+        println(INDENT + "5) " + "BySourceTest2");
+    }
+    
+    protected boolean runOption(int choice, String[] args) throws Exception {
+        switch (choice) {
+        case 1: SearchUtilsTest.main(args); return true;
+        case 2: RelationshipTest.main(args); return true;
+        case 3: RelationshipTest2.main(args); return true;
+        case 4: BySourceTest.main(args); return true;
+        case 5: BySourceTest2.main(args); return true;
+        default: return false;
+        }
+    }
+
+    public static void main(String[] args) {
+//        new PerformanceTest(3, args);
+        new PerformanceTest(0, args);
+    }    
+}
